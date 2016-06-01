a80a2263aa10c81e448d8feaacb4527ec806d862
napster <evgeniy.furmann@gmail.com>
2016-06-01 15:36:49 +0300

    delete project test

--- pages/settings_page.js
+++ pages/settings_page.js
@@ -12,6 +12,22 @@ var settings_page = function () {
         var consumer = element(by.xpath('/html/body/div/ui-view/ui-view/div/ui-view/ui-view/div[2]/div[2]/div[2]/div/div[2]/div/a[1]')).getText();
         expect(consumer).toBe(description);
     };
+
+    this.deleteProject = function (text) {
+        browser.isElementPresent(by.buttonText('Delete project'));
+        element(by.buttonText('Delete project')).click();
+        element(by.model('model.code')).sendKeys(text);
+        element(by.buttonText('Delete')).click();
+    };
+
+    this.assertNewProjectPage = function () {
+        browser.sleep(500);
+        browser.ignoreSynchronization = true;
+        browser.isElementPresent(by.buttonText('Submit'));
+    };
 };
 
 module.exports = new settings_page();
+
+
+
--- tests/Settings.js
+++ tests/Settings.js
@@ -21,4 +21,15 @@ describe('Gandalf', function () {
         var settings_page = new_project_page.getSettings();
         settings_page.addConsumer('newConsumer');
     });
+
+    it('DeleteProject test', function () {
+        home_page.fillSignUpForm(faker.name.findName(), faker.name.findName(), faker.internet.email());
+        var welcome_page = home_page.welcomePage();
+        var new_project_page = welcome_page.clickContinue();
+        new_project_page.createNewProject();
+        new_project_page.createProject('testProject', 'testDescription');
+        var settings_page = new_project_page.getSettings();
+        settings_page.deleteProject('DELETE');
+        settings_page.assertNewProjectPage();
+    });
 });
