770d05d8bafca9f10be992a9523f046df29e10c5
napster <evgeniy.furmann@gmail.com>
2016-06-29 16:55:17 +0300

    change user

--- tests/SignIn.js
+++ tests/SignIn.js
@@ -13,7 +13,7 @@ describe('Gandalf', function () {
     var home_page = require("../pages/home_page.js");
 
     it('SingIn test', function () {
-        home_page.fillSignInForm('furman', 'gt40vt14d');
+        home_page.fillSignInForm('Furmanjmx1', 'Gt40vt14d');
         var dashboard_page = home_page.submitForm();
         dashboard_page.assertDashboard();
     });
